e681b74fa0c6353004486d1170bfa2fc0d665005
rickchung <rickjung88@gmail.com>
2016-01-25 21:31:27 +0800

    Add modal draft

--- django_notebox/catear/notebox/models.py
+++ django_notebox/catear/notebox/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Song(models.Model):
+    upload_user  = models.ForeignKey('auth.User', related_name='song_upload_user')
+
+    title = models.CharField(max_length=200)
+    desc = models.TextField(default=None)
+    note = models.TextField()    
+    time_length = models.IntegerField()
+    song_img = ImageField(default=None)
+    song_img_url = models.URLField()
+    url = models.URLField()
+
+    modify_time = models.DateTimeField(auto_now=True)
+    upload_time = models.DateTimeField(auto_now_add=True)
+
+class Member(models.Model):
+    name = models.CharField(max_length=128)
+    email = models.EmailField()
+    phone = models.CharField(max_length=32, default=None)
+    address = models.CharField(max_length=128, default=None)
+    member_type = models.CharField(max_length=32, default=None)
+    profile_photo = ImageField(default=None)
+    modify_time = models.DateTimeField(auto_now=True)
+    register_time = models.DateTimeField(auto_now_add=True)
